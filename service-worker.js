2eece4210da218ae71da1027ae86116b4befae7b
770694baney <770694@qq.com>
2026-02-06 23:00:17 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// 缓存名称
+const CACHE_NAME = 'qipairoom-cache-v1';
+
+// 需要缓存的资源
+const urlsToCache = [
+  '棋牌室借款记账.html',
+  'manifest.json',
+  'icon.png'
+];
+
+// 安装service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截请求并返回缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
